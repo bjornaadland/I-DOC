09f6c7744ba11b54af2bf503f47dd91490d631fc
Per Kristian Gjermshus <perkristian@smarterphone.com>
2014-12-02 13:14:23 +0100

    Add preferences.

---
+++ android/app/src/main/java/no/nhc/i_doc/SettingsActivity.java
@@ -0,0 +1,16 @@
+package no.nhc.i_doc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {        
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+            .replace(android.R.id.content, new SettingsFragment())
+            .commit();
+
+    }
+}
