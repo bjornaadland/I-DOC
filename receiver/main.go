fd0fceb7cfe93f82cbc350c5d195b69692b6a48c
Per Kristian Gjermshus <perkristian@gjermshus.com>
2014-12-09 15:01:05 +0100

    Add a simple http receiver.

---
+++ receiver/main.go
@@ -0,0 +1,32 @@
+// 
+// Very simple file receiver and server.
+//
+package main
+import (
+	"github.com/twinj/uuid"
+	"os"
+	"io"
+	"net/http"
+)
+
+func newDocHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		u := uuid.NewV4()
+		filename := "/tmp/"+u.String()+".jpg"
+		outfile, err := os.OpenFile(filename, os.O_CREATE | os.O_WRONLY, 0666)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		io.Copy(outfile, req.Body)
+		w.Header().Add("Location", u.String())
+		w.WriteHeader(http.StatusCreated)
+	} else {
+		http.Error(w, "invalid method", http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/new-doc", newDocHandler)
+	http.ListenAndServe(":8080", nil)
+}
