e60f4ed483a741380b9a80d7db959473dbca1756
bjorn Aadland <bjorn.aadland@gmail.com>
2014-12-11 15:31:38 +0100

    Import images and videos from gallery

--- android/app/src/main/java/no/nhc/i_doc/CaptureFragment.java
+++ android/app/src/main/java/no/nhc/i_doc/CaptureFragment.java
@@ -1,7 +1,10 @@
 package no.nhc.i_doc;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
@@ -16,9 +19,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,7 +32,8 @@ public class CaptureFragment extends Fragment {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_VIDEO_CAPTURE = 2;
-
+    static final int REQUEST_IMAGE_IMPORT = 3;
+    static final int REQUEST_VIDEO_IMPORT = 4;
 
     public CaptureFragment() {
         setRetainInstance(true);
@@ -58,10 +64,10 @@ public class CaptureFragment extends Fragment {
                 }
             });
 
-        text = (TextView) fragmentView.findViewById(R.id.text_record_sound);
+        text = (TextView) fragmentView.findViewById(R.id.text_import_media);
         text.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    dispatchRecordSoundIntent();
+                    handleImportMedia();
                 }
             });
 
@@ -144,11 +150,37 @@ public class CaptureFragment extends Fragment {
         }
     }
     
-    public void dispatchRecordSoundIntent() {
-        /* test creating an empty document */
-        DocumentDB db = DocumentDB.get(getActivity());
-        Document doc = db.createDocument();
-        db.saveDocument(doc);
+    public void handleImportMedia() {
+        final CharSequence[] items = {
+                "Picture", "Video"
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Select media type");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Intent i=  new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (item == 0) {
+                    i.setType("image/*");
+                    startActivityForResult(i, REQUEST_IMAGE_IMPORT);
+                } else {
+                    i.setType("video/*");
+                    startActivityForResult(i, REQUEST_VIDEO_IMPORT);
+                }
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void fileCopy(File src, File dst) throws IOException {
+        FileInputStream inStream = new FileInputStream(src);
+        FileOutputStream outStream = new FileOutputStream(dst);
+        FileChannel inChannel = inStream.getChannel();
+        FileChannel outChannel = outStream.getChannel();
+        inChannel.transferTo(0, inChannel.size(), outChannel);
+        inStream.close();
+        outStream.close();
     }
 
     @Override
@@ -163,6 +195,35 @@ public class CaptureFragment extends Fragment {
         case REQUEST_IMAGE_CAPTURE:
         case REQUEST_VIDEO_CAPTURE:
             break;
+        case REQUEST_IMAGE_IMPORT:
+        case REQUEST_VIDEO_IMPORT: {
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getActivity().getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String mediaPath = c.getString(columnIndex);
+            c.close();
+            if (mediaPath == null) {
+                return;
+            }
+            if (requestCode == REQUEST_IMAGE_IMPORT) {
+                createImageFile();
+                try {
+                    fileCopy(new File(mediaPath), new File(mCurrentPhotoPath.substring(7)));
+                } catch (IOException e) {
+                    return;
+                }
+            } else if (requestCode == REQUEST_VIDEO_IMPORT) {
+                createVideoFile();
+                try {
+                    fileCopy(new File(mediaPath), new File(mCurrentVideoPath.substring(7)));
+                } catch (IOException e) {
+                    return;
+                }
+            }
+            break;
+        }
         default:
             return;
         }
@@ -172,11 +233,13 @@ public class CaptureFragment extends Fragment {
         DocumentUtils.SetDefaultTitle(doc);
 
         switch (requestCode) {
-        case REQUEST_IMAGE_CAPTURE: {
+        case REQUEST_IMAGE_CAPTURE:
+        case REQUEST_IMAGE_IMPORT: {
             doc.addFile(mCurrentPhotoPath);
             break;
         }
-        case REQUEST_VIDEO_CAPTURE: {
+        case REQUEST_VIDEO_CAPTURE:
+        case REQUEST_VIDEO_IMPORT: {
             doc.addFile(mCurrentVideoPath);
 
             // Create and store a thumbnail
@@ -191,6 +254,4 @@ public class CaptureFragment extends Fragment {
         }}
         db.saveDocument(doc);
     }
-
-
 }
